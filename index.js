40c84e27e57090633e3f16c231bcfd68d352067c
DoSome2022 <DoSomeWork2022@gmail.com>
2023-05-03 17:19:01 +0800

    in newmain

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const app = express();
 
 const port = 3000;
 
+const homeroutes = require('./router/home.js');
+
+app.use('/',homeroutes)
+
 app.listen(port , console.log(`server is running ${port}`))
